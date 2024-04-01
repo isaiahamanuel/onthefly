1249ac04a080ecf4ae5cf24ab43fa3363b8bbef7
Isaiah Amanuel <isaiahamanuel@gmail.com>
2024-04-01 10:32:34 -0700

    update server routing

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 import express from "express";
 import cors from "cors";
+import tripRoutes from "./routes/trips.js";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 app.get("/", (req, res) => {
   res
     .status(200)
     .send(
-      '<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>'
+      '<h1 style="text-align: center; margin-top: 50px;">✈️ OnTheFly API</h1>'
     );
 });
+
+app.use("api/trips", tripRoutes);
 
 const PORT = process.env.PORT || 3001;
 
