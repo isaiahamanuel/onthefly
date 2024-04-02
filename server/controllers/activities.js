d8f063ddf274ebff69acb8b77defc7b19a4c771e
Isaiah Amanuel <isaiahamanuel@gmail.com>
2024-04-02 11:37:18 -0700

    activity route fixes

--- server/controllers/activities.js
+++ server/controllers/activities.js
@@ -2,7 +2,8 @@ import { pool } from "../config/database.js";
 
 const createActivity = async (req, res) => {
   try {
-    const { trip_id, activity, num_votes } = req.body;
+    const { activity, num_votes } = req.body;
+    const trip_id = parseInt(req.params.trip_id);
     const results = await pool.query(
       `INSERT INTO activities (trip_id, activity, num_votes)
       VALUES($1, $2, $3)
@@ -26,12 +27,13 @@ const getActivities = async (req, res) => {
     res.status(409).json({ error: error.message });
   }
 };
-const getActivity = async (req, res) => {
+const getTripActivities = async (req, res) => {
   try {
-    const id = parseInt(req.params.id);
-    const results = await pool.query("SELECT * FROM activities WHERE id = $1", [
-      id,
-    ]);
+    const trip_id = parseInt(req.params.id);
+    const results = await pool.query(
+      "SELECT * FROM activities WHERE trip_id = $1",
+      [trip_id]
+    );
     res.status(200).json(results.rows[0]);
   } catch (error) {
     res.status(409).json({ error: error.message });
@@ -39,16 +41,16 @@ const getActivity = async (req, res) => {
     console.log("Error:", error.message);
   }
 };
-const updateActivity = async (request, response) => {
+const updateActivityLikes = async (request, response) => {
   try {
-    const { trip_id, activity, num_votes } = req.body;
+    const { activity, num_votes } = req.body;
     const id = parseInt(req.params.id);
 
     const results = await pool.query(
       `UPDATE activities
-      SET activity = $1, num_votes = $2
-      WHERE id = $3`,
-      [activity, num_votes, id]
+      SET num_votes = $1
+      WHERE id = $2`,
+      [num_votes, id]
     );
 
     res.status(200).json(results.rows);
@@ -72,7 +74,7 @@ const deleteActivity = async (req, res) => {
 export default {
   createActivity,
   getActivities,
-  getActivity,
-  updateActivity,
+  getTripActivities,
+  updateActivityLikes,
   deleteActivity,
 };
--- server/routes/activities.js
+++ server/routes/activities.js
@@ -4,9 +4,9 @@ import ActivitiesController from "../controllers/activities.js";
 const router = express.Router();
 
 router.get("/", ActivitiesController.getActivities);
-router.get("/:id", ActivitiesController.getActivity);
-router.post("/", ActivitiesController.createActivity);
+router.get(":trip_id", ActivitiesController.getTripActivities);
+router.post("/:trip_id", ActivitiesController.createActivity);
 router.delete("/:id", ActivitiesController.deleteActivity);
-router.patch("/:id", ActivitiesController.updateActivity);
+router.patch("/:id", ActivitiesController.updateActivityLikes);
 
 export default router;
